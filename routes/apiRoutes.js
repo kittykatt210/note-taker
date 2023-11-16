2f5f779df7a0269f3dfae1a9270bba7871f2eeb7
kittykatt210 <keperry30@icloud.com>
2023-11-15 21:17:58 -0600

    Created heroku deployment site.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,17 @@
 const router = require('express').Router();
+const path = require('path');
 const fs = require('fs');
 var uniqid = require('uniqid');
 
 router.get('/api/notes', async (req, res) => {
-    const db = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
-    res.json(db);
+   res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 router.post('/api/notes', (req, res) => {
     const db = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
-    const { title, text } = req.body;
-    
     const newNote = {
-        title,
-        text,
+        title: req.body.title,
+        text: req.body.text,
         id: uniqid(),
     };
 
